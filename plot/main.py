5ce4760dff7139ab7e94b65468f9c6872a391b46
Masaaki Miyashita <m.mysht118@gmail.com>
2026-04-08 13:32:51 +0900

    upd.

--- plot/main.py
+++ plot/main.py
@@ -1,3 +1,4 @@
+import altair as alt
 import polars as pl
 
 
@@ -8,8 +9,17 @@ def main():
             ddf = pl.read_csv(f"../test/sample-result/twitter-{ns}-{ps}_time.csv")
             dfs.append(ddf.with_columns([pl.lit(ns).alias("n"), pl.lit(ps).alias("p")]))
 
-    df = pl.concat(dfs)
-    print(df)
+    df: pl.DataFrame = pl.concat(dfs).rename({"time(us)": "time"})
+    print(df.filter(pl.col("time") == 0))
+
+    chart = (
+        alt.Chart(df.filter(pl.col("method") != "MCL"))
+        .mark_point()
+        .encode(x="n", y=alt.Y("time").scale(type="log"), color="method")
+        # .properties(width=160, height=160)
+        .facet(column="p")
+    )
+    chart.save("plot.png")
 
 
 if __name__ == "__main__":
